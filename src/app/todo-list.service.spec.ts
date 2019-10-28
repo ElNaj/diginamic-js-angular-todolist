62d45f7a577e90d7d56f40eb97a814c25b0b6473
ElNajim <nelfateoui@gmail.com>
2019-10-28 17:42:54 +0100

    Tp Angular : toDoList fonctionnelle

--- src/app/todo-list.service.spec.ts
+++ src/app/todo-list.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { TodoListService } from './todo-list.service';
 
 describe('TodoListServiceService', () => {
+  
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
